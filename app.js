e4e7f9f7ed77a8f65dbd46b639364ace1f24c08f
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2022-10-19 21:46:26 -0700

    react js fundemental

---
+++ app.js
@@ -0,0 +1,40 @@
+1.//How to declare let and constant variable
+
+const myName='Seraj';//-----vraible can not be changrd
+
+let shop='Macdonald';//-------only value change
+shop='Cats and Eye';
+
+2.// The six basic condition : > , <, ===, !==,  >= ,<=
+
+3.//Multiple condition: && and ||
+
+/*if(){
+
+}else{
+
+}*/
+
+4. //Array, index , length, push,slice, pop--
+
+const numbers=[78,89,90,67]
+numbers[0]=99 // this is change of element of indexposition
+//console.log(numbers);
+
+5. //forloop and sometimes while loop
+
+for(i=0;i<numbers.length;i++){
+   const number=numbers[i]
+   console.log(number);
+}
+
+6.// function call
+
+   function multiply(num1,num2){
+      const result= num1* num2
+      return result
+   }
+
+   const output=multiply(5,6)
+
+   7. // object--- three ways to access object
